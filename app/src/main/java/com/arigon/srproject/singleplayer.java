bfc0e60a392d123acec7aae33972b3bd92529baf
Arturo Gonzalez <arturo.isaac1481@gmail.com>
2017-02-05 13:14:32 -0600

    single player is almost working, can randomly generate working game board

--- app/src/main/java/com/arigon/srproject/singleplayer.java
+++ app/src/main/java/com/arigon/srproject/singleplayer.java
@@ -24,6 +24,14 @@ public class singleplayer extends AppCompatActivity {
     int turn = 1;
 
     Button[] boardButtons = new Button[49];//array of board buttons
+    Button[] removedButtons = new Button[49];//array of removed buttons
+    int[] buttonIDs = new int[]{R.id.btn1,R.id.btn2,R.id.btn3,R.id.btn4,R.id.btn5,R.id.btn6,
+            R.id.btn7,R.id.btn8,R.id.btn9,R.id.btn10,R.id.btn11,R.id.btn12,R.id.btn13,
+            R.id.btn14,R.id.btn15,R.id.btn16,R.id.btn17,R.id.btn18,R.id.btn19,R.id.btn20,R.id.btn21,
+            R.id.btn22,R.id.btn23,R.id.btn24,R.id.btn25,R.id.btn26,R.id.btn27,R.id.btn28,R.id.btn29,
+            R.id.btn30,R.id.btn31,R.id.btn32,R.id.btn33,R.id.btn34,R.id.btn35,R.id.btn36,R.id.btn37,
+            R.id.btn38,R.id.btn39,R.id.btn40,R.id.btn41,R.id.btn42,R.id.btn43,R.id.btn44,R.id.btn45,
+            R.id.btn46,R.id.btn47,R.id.btn48,R.id.btn49};//array of ids
 
 
     //this function runs when the program starts
@@ -970,9 +978,8 @@ public class singleplayer extends AppCompatActivity {
         //inequality signs are placed between numbers if accepted
         ////////////////////////////////////////////////
         while (!acceptable) {
-            //acceptable = true;
             int errors = 0;
-            //array to contain rancom numbers
+            //array to contain random numbers
             Integer[] arr = new Integer[25];
             for (int i = 0; i < arr.length; i++) {
                 arr[i] = i + 1;
@@ -1048,7 +1055,29 @@ public class singleplayer extends AppCompatActivity {
                 acceptable = true;
             }
         }
+
+
+        //remove numbers from board
+        for(int i=1; i<50; i++)
+        {
+            if(i%2 ==1)//check if it is a light button(odd number on board)
+            {
+                //add to array of buttons
+                Button btn = (Button) findViewById(buttonIDs[i-1]);
+                removedButtons[i-1]=btn;//set selected button to removedButtons array
+                //delete text from some buttons
+                String v;
+                int x = (int) (Math.random() * 2);
+                if(x == 1)
+                    boardButtons[i-1].setText("");
+
+
+
+            }
+        }
+
     }
 }
+
 
 
