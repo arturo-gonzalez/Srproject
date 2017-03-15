e1e23667bdcc1aa02437b341c1fb0ac0311c0575
Fernando Martinez <masterlucas176@gmail.com>
2017-03-15 15:37:59 -0500

    fixed the nullpointer crash

--- app/src/main/java/com/arigon/srproject/twoplayer.java
+++ app/src/main/java/com/arigon/srproject/twoplayer.java
@@ -6,6 +6,7 @@ package com.arigon.srproject;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
