7c11c844bc05bcc45b19d4381bc68cbba9aecde7
Fernando Martinez <masterlucas176@gmail.com>
2017-03-16 14:08:15 -0500

    check2 2.0

--- app/src/main/java/com/arigon/srproject/check2.java
+++ app/src/main/java/com/arigon/srproject/check2.java
@@ -14,25 +14,25 @@ public class check2{
     public Boolean checkIfValid(SquareButton button, TextView alert, SquareButton[][] boardButtons, String value) {
         int row = button.row;
         int column = button.column;
-        if(row - 2 > 0) {
+        if(row - 2 > 0 && boardButtons[row - 2][column].getText().toString().length() > 0) {
             if(boardButtons[row - 1][column].getText() == "<" && Integer.parseInt(boardButtons[row-2][column].getText().toString()) >= Integer.parseInt(value)
                     || boardButtons[row - 1][column].getText() == ">" && Integer.parseInt(boardButtons[row-2][column].getText().toString()) <= Integer.parseInt(value)) {
                 return false;
             }
         }
-        if(column - 2 > 0){
+        if(column - 2 > 0 && boardButtons[row][column - 2].getText().toString().length() > 0){
             if(boardButtons[row][column - 1].getText() == "<" && Integer.parseInt(boardButtons[row][column - 2].getText().toString()) >= Integer.parseInt(value)
                     || boardButtons[row][column - 1].getText() == ">" && Integer.parseInt(boardButtons[row][column - 2].getText().toString()) <= Integer.parseInt(value)) {
                 return false;
             }
         }
-        if(row + 2 < boardButtons.length){
+        if(row + 2 < boardButtons.length && boardButtons[row + 2][column].getText().toString().length() > 0){
             if(boardButtons[row + 1][column].getText() == "<" && Integer.parseInt(boardButtons[row + 2][column].getText().toString()) <= Integer.parseInt(value)
                     || boardButtons[row + 1][column].getText() == ">" && Integer.parseInt(boardButtons[row + 2][column].getText().toString()) >= Integer.parseInt(value)) {
                 return false;
             }
         }
-        if(column - 2 < boardButtons[row].length){
+        if(column - 2 < boardButtons[row].length && boardButtons[row][column + 2].getText().toString().length() > 0){
             if(boardButtons[row][column + 1].getText() == "<" && Integer.parseInt(boardButtons[row][column + 2].getText().toString()) <= Integer.parseInt(value)
                     || boardButtons[row][column + 1].getText() == ">" && Integer.parseInt(boardButtons[row][column + 2].getText().toString()) >= Integer.parseInt(value)) {
                 return false;
