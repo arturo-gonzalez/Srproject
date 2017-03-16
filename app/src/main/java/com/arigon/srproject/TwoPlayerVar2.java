517cef44eb0e4182984a836e7afed7549402f532
Fernando Martinez <masterlucas176@gmail.com>
2017-03-16 14:28:42 -0500

    check2 2.0

--- app/src/main/java/com/arigon/srproject/TwoPlayerVar2.java
+++ app/src/main/java/com/arigon/srproject/TwoPlayerVar2.java
@@ -90,7 +90,7 @@ public class TwoPlayerVar2 extends AppCompatActivity {
                         @Override
                         public void onClick(View v) {
                             SquareButton button = (SquareButton) v;
-                            if (c.checkIfValid(button, alert, boardButtons, value)) {
+                            if (c.checkIfValid(button, boardButtons, value)) {
                                 button.setText(value);
                             }
                         }
