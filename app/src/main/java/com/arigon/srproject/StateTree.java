7c57f6e750bf6d11df928308d99c419cf2c951b3
Fernando Martinez <masterlucas176@gmail.com>
2017-05-06 20:49:14 -0500

    Monte Carlo TS like 40% done

--- app/src/main/java/com/arigon/srproject/StateTree.java
+++ app/src/main/java/com/arigon/srproject/StateTree.java
@@ -8,21 +8,34 @@ import java.util.List;
  */
 
 public class StateTree {
-    private StateTreeNode root;
+    public StateTreeNode root;
 
-    public StateTree(BoardState rootData){
-        root = new StateTreeNode();
-        root.board = rootData;
-        root.children = new ArrayList<StateTreeNode>();
-        root.visits = 0;
-        root.score = 0;
+    //constructor for the tree, creates a root with given data
+    public StateTree(BoardState rootData, int inPlayer){
+        root = new StateTreeNode(rootData, inPlayer);
     }
 
+
     public static class StateTreeNode{
-        public BoardState board;
+
+        //BoardState is an object that contains a 2D array with the board
+        //and a 1D array for the numberboard
+        public BoardState game;
+        public int player;
         public StateTreeNode parent;
-        public List<StateTreeNode> children;
+        public ArrayList<StateTreeNode> children;
         public int visits;
         public double score;
+
+        //constructor for node, sets up the data
+        public StateTreeNode(BoardState data, int inPlayer)
+        {
+            game = data;
+            player = inPlayer;
+            parent = null;
+            children = new ArrayList<StateTreeNode>();
+            visits = 0;
+            score = 0;
+        }
     }
 }
